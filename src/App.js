f47642ca60a726ef4b999b51470102e5b4b82d46
Malik Hamza <malikhamza.v@gmail.com>
2023-01-08 18:45:57 +0500

    complete landing page

---
+++ src/App.js
@@ -0,0 +1,34 @@
+/* eslint-disable quotes */
+import React from "react";
+
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+
+import "./App.css";
+import Placeholder from "./components/placeholder/Placeholder";
+
+const App = () => (
+  <div className="App">
+    <Placeholder />
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
+    </div>
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
+
+export default App;
